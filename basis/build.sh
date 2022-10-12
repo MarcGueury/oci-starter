4ccdc8686f442e184ef44ecbad306328f7ded6d9
Marc Gueury <marc.gueury@oracle.com>
2022-10-12 17:14:51 +0200

    2022-10-12_17-14

--- basis/build.sh
+++ basis/build.sh
@@ -1,8 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-. bin/env_pre_terraform.sh
+# Needed to get the TF_VAR_prefix
+. variables.sh
 . bin/sshkey_generate.sh
+. bin/env_pre_terraform.sh
 app_src/build_app.sh compute
 ui_src/build_ui.sh compute
 bin/terraform.sh
