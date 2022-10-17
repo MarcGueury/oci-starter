bd44e5ddda3ff57d6197761c0ca80e1bd7cc9736
Marc Gueury <marc.gueury@oracle.com>
2022-10-17 19:57:52 +0200

    2022-10-17_19-57

--- option/terraform/mysql_common.tf
+++ option/terraform/mysql_common.tf
@@ -1,7 +1,7 @@
 locals {
     db_ip = data.oci_mysql_mysql_db_system.starter_mysql.endpoints.0.ip_address 
     db_port = data.oci_mysql_mysql_db_system.starter_mysql.endpoints.0.port
-    db_url = format("%s:%s", db_ip, db_port)
+    db_url = format("%s:%s", local.db_ip, local.db_port)
     // jdbc:mysql://10.1.1.237/db1?user=root&password=xxxxx
-    jdbc_url = format("jdbc:mysql://%s/db1", mysql_ip)
+    jdbc_url = format("jdbc:mysql://%s/db1", local.db_ip)
 }
