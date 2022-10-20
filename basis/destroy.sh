d54c570ded98839ca61505028fa70a0edd766ca1
Marc Gueury <marc.gueury@oracle.com>
2022-10-20 08:36:12 +0200

    2022-10-20_08-36

--- basis/destroy.sh
+++ basis/destroy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# The first parameter is passed to terraform destroy to allow to use --auto-approve
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+# Call the script with --auto-approve to destroy without prompt
+
 . bin/env_pre_terraform.sh
+if [ -d oke ]; then
+  oke/oke_destroy.sh $1
+fi
 
 cd terraform
 terraform destroy $1
---
+++ option/oke/oke_destroy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/..
+
+echo "OKE DESTROY"
+
+if [ $1 != "--auto-approve" ]; then
+  read -p "Do you want to proceed? (yes/no) " yn
+
+  case $yn in 
+  	yes ) echo ok, we will proceed;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+  esac
+fi
+
+export KUBECONFIG=terraform/starter_cluster_kubeconfig
+kubectl delete ingress,services --all
