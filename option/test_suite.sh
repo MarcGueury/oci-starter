49e6d17c547e18bbdc351eedcfb6895817f4658b
Marc Gueury <marc.gueury@oracle.com>
2022-10-20 21:23:39 +0200

    2022-10-20_21-23

--- option/test_suite.sh
+++ option/test_suite.sh
@@ -24,12 +24,11 @@ build_test_destroy () {
   echo "-- Build test $TEST_NAME ---------------------------------------"   
   SECONDS=0
   pwd
-  cd $SCRIPT_DIR/test/$TEST_NAME/output
+  TEST_DIR=$SCRIPT_DIR/test/$TEST_NAME
+  cd $TEST_DIR/output
   pwd
   ./build.sh > build.log 2>&1  
-  cp /tmp/result.html $SCRIPT_DIR/test/${TEST_NAME}_result.html
-  cp /tmp/result.json $SCRIPT_DIR/test/${TEST_NAME}_result.json
-  echo "build_secs=" $SECONDS >  $SCRIPT_DIR/test/${TEST_NAME}_time.txt
+  echo "build_secs=" $SECONDS >  ${TEST_DIR}_time.txt
   if grep -q "OCI Starter" /tmp/result.html; then
      echo RESULT HTML: OK 
   else
@@ -40,9 +39,11 @@ build_test_destroy () {
   else
      echo RESULT JSON: ***** BAD ******
   fi
+  mv /tmp/result.html ${TEST_DIR}_result.html
+  mv /tmp/result.json ${TEST_DIR}_result.json
   SECONDS=0
   ./destroy.sh --auto-approve > destroy.log 2>&1  
-  echo "destroy_secs=" $SECONDS >> $SCRIPT_DIR/test/${TEST_NAME}_time.txt
+  echo "destroy_secs=" $SECONDS >> ${TEST_DIR}_time.txt
 }
 
 if [ -d test ]; then
@@ -61,8 +62,6 @@ git clone https://github.com/MarcGueury/oci-starter
 start_test 01_JAVA_HELIDON_COMPUTE_ATP
 ./oci_starter.sh -language java -deploy compute -db_password $TEST_DB_PASSWORD > $SCRIPT_DIR/test/${TEST_NAME}.log 2>&1  
 build_test_destroy
-
-exit
 
 # Java Compute ATP + Existing Subnet
 start_test 02_JAVA_HELIDON_COMPUTE_ATP_EX_SUBNET
