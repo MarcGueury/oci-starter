c0ef7cd81f727dc552fc341e8b1858f328b54bc3
Marc Gueury <marc.gueury@oracle.com>
2022-11-03 19:02:23 +0100

    2022-11-03_19-02

--- basis/bin/deploy_bastion.sh
+++ basis/bin/deploy_bastion.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Using RSYNC allow to reapply the same command several times easily
 rsync -av -e "ssh -o StrictHostKeyChecking=no -i id_devops_rsa" db_src opc@$BASTION_IP:.
-ssh -o StrictHostKeyChecking=no -i id_devops_rsa opc@$BASTION_IP "export DB_USER=\"$TF_VAR_db_user\";export DB_PASSWORD=\"$TF_VAR_db_password\";export DB_URL=\"$DB_URL\"; bash db_src/db_init.sh > db_src/db_init.log 2>&1"
+ssh -o StrictHostKeyChecking=no -i id_devops_rsa opc@$BASTION_IP "export DB_USER=\"$TF_VAR_db_user\";export DB_PASSWORD=\"$TF_VAR_db_password\";export DB_URL=\"$DB_URL\"; bash db_src/db_init.sh 2>&1 | tee -a db_src/db_init.log"
 
--- basis/bin/deploy_compute.sh
+++ basis/bin/deploy_compute.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Using RSYNC allow to reapply the same command several times easily
 rsync -av -e "ssh -o StrictHostKeyChecking=no -i id_devops_rsa" compute/* opc@$COMPUTE_IP:.
-ssh -o StrictHostKeyChecking=no -i id_devops_rsa opc@$COMPUTE_IP "export TF_VAR_java_version=\"$TF_VAR_java_version\";export TF_VAR_language=\"$TF_VAR_language\";export JDBC_URL=\"$JDBC_URL\";export DB_URL=\"$DB_URL\";bash compute_bootstrap.sh > compute_bootstrap.log 2>&1"
+ssh -o StrictHostKeyChecking=no -i id_devops_rsa opc@$COMPUTE_IP "export TF_VAR_java_version=\"$TF_VAR_java_version\";export TF_VAR_language=\"$TF_VAR_language\";export JDBC_URL=\"$JDBC_URL\";export DB_URL=\"$DB_URL\";bash compute_bootstrap.sh 2>&1 | tee -a compute_bootstrap.log"
