924e767dad940019f7e43cd45c2b1b191d6ea142
Marc Gueury <marc.gueury@oracle.com>
2022-11-09 18:52:55 +0100

    2022-11-09_18-52

--- basis/bin/deploy_bastion.sh
+++ basis/bin/deploy_bastion.sh
@@ -3,7 +3,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 . env.sh
 
-# Using RSYNC allow to reapply the same command several times easily
-rsync -av -e "ssh -o StrictHostKeyChecking=no -i id_devops_rsa" db_src opc@$BASTION_IP:.
+# Using RSYNC allow to reapply the same command several times easily. 
+if command -v rsync &> /dev/null
+then
+  rsync -av -e "ssh -o StrictHostKeyChecking=no -i id_devops_rsa" db_src opc@$BASTION_IP:.
+else
+  scp -r -o StrictHostKeyChecking=no -i id_devops_rsa db_src opc@$BASTION_IP:/home/opc/.
+fi
 ssh -o StrictHostKeyChecking=no -i id_devops_rsa opc@$BASTION_IP "export DB_USER=\"$TF_VAR_db_user\";export DB_PASSWORD=\"$TF_VAR_db_password\";export DB_URL=\"$DB_URL\"; bash db_src/db_init.sh 2>&1 | tee -a db_src/db_init.log"
 
