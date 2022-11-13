d8ee2711e5ba4fd7cbffe69f0c2c82a1269897d0
Marc Gueury <marc.gueury@oracle.com>
2022-11-13 16:24:48 +0100

    2022-11-13_16-24

--- option/app_src/fn/fn_java_mysql/src/main/java/com/example/fn/HelloFunction.java
+++ option/app_src/fn/fn_java_mysql/src/main/java/com/example/fn/HelloFunction.java
@@ -12,34 +12,16 @@ public class HelloFunction {
   private final String dbPassword = System.getenv().get("DB_PASSWORD");
   private final String dbUrl = System.getenv().get("DB_URL");
 
-  final static String CONN_FACTORY_CLASS_NAME = "oracle.jdbc.pool.OracleDataSource";
-
-  public HelloFunction() {
-    System.out.println("Setting up pool data source");
-    poolDataSource = PoolDataSourceFactory.getPoolDataSource();
-    try {
-      poolDataSource.setConnectionFactoryClassName(CONN_FACTORY_CLASS_NAME);
-      poolDataSource.setURL(dbUrl);
-      poolDataSource.setUser(dbUser);
-      poolDataSource.setPassword(dbPassword);
-      poolDataSource.setConnectionPoolName("UCP_POOL");
-    } catch (SQLException e) {
-      System.out.println("Pool data source error!");
-      e.printStackTrace();
-    }
-    System.out.println("Pool data source setup...");
-    System.setProperty("oracle.jdbc.fanEnabled", "false");
-  }
+  public HelloFunction() {}
 
   public String handleRequest(String input) {
-    // System.out.println("dbUser=" + dbUser + " / dbPassword=" + dbPassword + " / dbUurl=" + dbUrl);
     int counter = 0;
     StringBuffer sb = new StringBuffer();
     sb.append("[");
     try {
       System.out.println("Before classForName");
-      Class.forName("oracle.jdbc.driver.OracleDriver");
-      Connection conn = poolDataSource.getConnection();
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
       System.out.println("After connection");
       Statement stmt = conn.createStatement();
       ResultSet rs = stmt.executeQuery("SELECT * FROM dept");
