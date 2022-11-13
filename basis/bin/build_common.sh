d501eff72543841a8993c9ae8291601b04d17020
Marc Gueury <marc.gueury@oracle.com>
2022-11-13 22:43:19 +0100

    2022-11-13_22-43

--- basis/bin/build_common.sh
+++ basis/bin/build_common.sh
@@ -25,6 +25,24 @@ build_ui() {
 }
 
 build_function() {
+  # Build the function
+  fn create context ${TF_VAR_region} --provider oracle
+  fn use context ${TF_VAR_region}
+  fn update context oracle.compartment-id ${TF_VAR_compartment_ocid}
+  fn update context api-url https://functions.${TF_VAR_region}.oraclecloud.com
+  fn update context registry ${TF_VAR_ocir}/${TF_VAR_namespace}
+  fn build --verbose | tee > $TMP_DIR/fn_build.log
+  if grep --quiet "built successfully" $TMP_DIR/fn_build.log; then
+     fn bump
+     # Store the image name and DB_URL in files
+     grep "built successfully" $TMP_DIR/fn_build.log | sed "s/Function //" | sed "s/ built successfully.//" > $TMP_DIR/fn_image.txt
+     echo "$1" > $TMP_DIR/fn_db_url.txt
+     . ../env.sh
+     # Push the image to docker
+     docker login ${TF_VAR_ocir} -u ${TF_VAR_namespace}/${TF_VAR_username} -p "${TF_VAR_auth_token}"
+     docker push $TF_VAR_fn_image
+  fi 
+
   # First create the Function using terraform
   # Run env.sh to get function image 
   cd $SCRIPT_DIR/..
--- option/app_src/fn/fn_node/build_app.sh
+++ option/app_src/fn/fn_node/build_app.sh
@@ -2,21 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/../bin/build_common.sh
 
-# Cloud Shell
-fn create context ${TF_VAR_region} --provider oracle
-fn use context ${TF_VAR_region}
-fn update context oracle.compartment-id ${TF_VAR_compartment_ocid}
-fn update context api-url https://functions.${TF_VAR_region}.oraclecloud.com
-fn update context registry ${TF_VAR_ocir}/${TF_VAR_namespace}
-fn build --verbose | tee > $TMP_DIR/fn_build.log
-if grep --quiet "built successfully" $TMP_DIR/fn_build.log; then
-   fn bump
-   grep "built successfully" $TMP_DIR/fn_build.log | sed "s/Function //" | sed "s/ built successfully.//" > $TMP_DIR/fn_image.txt
-   echo "$JDBC_URL" > $TMP_DIR/fn_db_url.txt
-   . ../env.sh
-   docker login ${TF_VAR_ocir} -u ${TF_VAR_namespace}/${TF_VAR_username} -p "${TF_VAR_auth_token}"
-   docker push $TF_VAR_fn_image
-fi 
 # fn -v deploy --app ${TF_VAR_prefix}-fn-application
 # fn invoke ${TF_VAR_prefix}-fn-application ${TF_VAR_prefix}-fn-function
 # fn config function ${TF_VAR_prefix}-fn-application fn-starter DB_USER $TF_VAR_db_user
@@ -26,5 +11,4 @@ fi
 
 # Deploy with terraform
 # Then Work-around: terraforms is not able to create a APIGW with dynamic multiple backends
-build_function
-
+build_function $DB_URL
