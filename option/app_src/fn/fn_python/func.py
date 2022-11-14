57c5af42d598b11a6e9300f9b304dc04241f967e
Marc Gueury <marc.gueury@oracle.com>
2022-11-14 11:06:04 +0100

    2022-11-14_11-06

--- option/app_src/fn/fn_python/func.py
+++ option/app_src/fn/fn_python/func.py
@@ -1,18 +1,13 @@
-#
-# oci-adb-client-runsql-python version 1.0.
-#
-# Copyright (c) 2020 Oracle, Inc.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#
-
 import io
 import json
 import logging
+import os
 import oracledb
 
 from fdk import response
 
 def handler(ctx, data: io.BytesIO = None):
+    a = []
     try:
         conn = oracledb.connect(
           user=os.getenv('DB_USER'),
@@ -24,11 +19,15 @@ def handler(ctx, data: io.BytesIO = None):
         for row in cursor.execute('select deptno, dname, loc from dept'):
             a.append( {"deptno": row[0], "dname": row[1], "loc": row[2]} )
         print(a)
-        response = jsonify(a)
-        response.status_code = 200
-        return response
     except Exception as e:
-        logging.getLogger().info('error: ' + str(ex))
+        logging.getLogger().info('error: ' + str(e))
     finally:
         cursor.close() 
         conn.close()     
+    return response.Response(
+        ctx, response_data=json.dumps(
+            {"message": "Hello"}),
+        headers={"Content-Type": "application/json"}
+    )
+
+    
