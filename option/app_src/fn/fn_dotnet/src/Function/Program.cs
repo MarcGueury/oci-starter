426e5bd076d786e671a1071f46f17eea939fda0e
Marc Gueury <marc.gueury@oracle.com>
2022-11-14 21:34:50 +0100

    2022-11-14_21-34

---
+++ option/app_src/fn/fn_dotnet/src/Function/Program.cs
@@ -0,0 +1,47 @@
+using Fnproject.Fn.Fdk;
+using Oracle.ManagedDataAccess.Client;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Function.Tests")]
+namespace Function {
+  class Starter {
+    public static string user = Environment.GetEnvironmentVariable("DB_USER");
+    public static string pwd = Environment.GetEnvironmentVariable("DB_PASSWORD");
+    public static string db = Environment.GetEnvironmentVariable("DB_URL");
+
+    public string greet(string input) {
+      string result = "[";
+      string conStringUser = "User Id=" + user + ";Password=" + pwd + ";Data Source=" + db + ";";
+
+      using (OracleConnection con = new OracleConnection(conStringUser))
+      {
+          using (OracleCommand cmd = con.CreateCommand())
+          {
+              try
+              {
+                  con.Open();
+                  Console.WriteLine("Successfully connected to Oracle Database as " + user);
+                  Console.WriteLine();
+
+                  //Retrieve sample data
+                  cmd.CommandText = "SELECT deptno, dname, loc FROM dept";
+                  OracleDataReader reader = cmd.ExecuteReader();
+                  while (reader.Read())
+                  {
+                    result += "{ \"deptno\": \"" +reader.GetString(0) + "\", \"dname\": \"" +reader.GetString(0) + "\", \"deptno\": \"" +reader.GetString(0) + + "\"}";
+                  }
+                  reader.Dispose();
+              }
+              catch (Exception ex)
+              {
+                  Console.WriteLine(ex.Message);
+              }
+          }
+      }
+      result += "]";
+      return result;
+    }
+
+    static void Main(string[] args) { Fdk.Handle(args[0]); }
+  }
+}
