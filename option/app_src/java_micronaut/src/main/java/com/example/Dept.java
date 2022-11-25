e9d91c2fa5e5dfc8efa89f973b72e75aec2537b5
Marc Gueury <marc.gueury@oracle.com>
2022-11-25 16:38:56 +0100

    2022-11-25_16-38

--- option/app_src/java_micronaut/src/main/java/com/example/Dept.java
+++ option/app_src/java_micronaut/src/main/java/com/example/Dept.java
@@ -1,18 +1,7 @@
 package com.example;
 
-import io.micronaut.serde.annotation.Serdeable;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import static javax.persistence.GenerationType.AUTO;
-
-@Serdeable
 @Entity
 @Table(name = "dept")
 public class Dept {
---
+++ option/app_src/java_micronaut/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+export DB_USER=##DB_USER##
+export DB_PASSWORD=##DB_PASSWORD##
+export JDBC_URL=##JDBC_URL##
+java -jar demo-0.1.jar > app.log 2>&1 
