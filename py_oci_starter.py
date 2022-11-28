c1b0fed3dc287b0140a5a92f4336c2268814ace9
Ewan Slater <ewan.slater@gmail.com>
2022-11-28 18:52:15 +0000

    started catching illegal values

--- py_oci_starter.py
+++ py_oci_starter.py
@@ -65,6 +65,53 @@ def allowed_options():
         + list(conditional_options.keys()) \
         + mandatory_options + no_default_options
 
+allowed_values = {
+    '-language': {'java','node','python','ords'}
+}
+
+def check_values():
+    illegals = {}
+    for arg in allowed_values:
+        arg_val = prog_arg_dict().get(arg)
+        if arg_val is not None:
+            if arg_val not in allowed_values[arg]:
+                illegals[arg] = arg_val
+    return illegals
+
+tf_var_map = {
+    'apigw_ocid': 'TF_VAR_apigw_ocid',
+    'auth_token': 'TF_VAR_auth_token',
+    'atp_ocid': 'TF_VAR_atp_ocid',
+    'bastion_ocid': 'TF_VAR_bastion_ocid',
+    'compartment_ocid': 'TF_VAR_compartment_ocid',
+    'database': 'TF_VAR_db_strategy',
+    'db_ocid': 'TF_VAR_db_ocid',
+    'db_password': 'TF_VAR_db_password',
+    'db_user': 'TF_VAR_db_user',
+    'deploy': 'TF_VAR_deploy_strategy',
+    'fnapp_ocid': 'TF_VAR_fnapp_ocid',
+    'java_framework': 'TF_VAR_java_framework',
+    'java_version': 'TF_VAR_java_version',
+    'java_vm': 'TF_VAR_java_vm',
+    'kubernetes': 'TF_VAR_kubernetes_strategy',
+    'language': 'TF_VAR_language',
+    'license': 'TF_VAR_license_model',
+    'mysql_ocid': 'TF_VAR_mysql_ocid',
+    'oke_ocid': 'TF_VAR_oke_ocid',
+    'prefix': 'TF_VAR_prefix',
+    'subnet_ocid': 'TF_VAR_subnet_ocid',
+    'ui': 'TF_VAR_ui_strategy',
+    'vcn_ocid': 'TF_VAR_vcn_ocid',
+}
+
+# db rules
+# deploy rules
+# java rules
+# k8s rules
+# mysql rules
+# ui rules
+# vcn rules
+
 def help():
     message = f'''
 Usage: {script_name()} [OPTIONS]
@@ -78,35 +125,37 @@ oci-starter.sh
    -java_vm (default jdk | graalvm)  
    -java_version (default 17 | 11 | 8)
    -kubernetes (default oke | docker) 
-   -oke_ocid ()
+   -oke_ocid (optional)
    -ui (default html | reactjs | jet | angular | none) 
-   -vcn_ocid()
-   -subnet_ocid()
+   -vcn_ocid (optional)
+   -subnet_ocid (optional)
    -database (default atp | dbsystem | mysql)
    -atp_ocid (optional)
    -db_ocid (optional)
    -mysql_ocid (optional)
    -db_user (default admin)
-   -db_password ( mandatory )
+   -db_password (mandatory)
 '''
     if len(unknown_params) > 0:
-        print('U')
         s = ' '
         for unknown in unknown_params:
             s += f'{unknown} '
-        message += f'unknown parameter(s):{s}\n'
+        message += f'Unknown parameter(s):{s}\n'
     if len(missing_params) > 0:
-        print('M')
         s = ' '
         for missing in missing_params:
             s += f'{missing} '
-        message += f'missing parameter(s):{s}\n'
+        message += f'Missing parameter(s):{s}\n'
     if len(missing_conditional_params) > 0:
-        print('C')
-        s = ' '
+        s = ''
         for key in missing_conditional_params:
-            s += f'{missing_conditional_params[key]} is mandatory with {key}\n'
-        message += f'missing parameters:{s}\n'
+            s += f'Missing parameter: {missing_conditional_params[key]} is mandatory with {key}\n'
+        message += s
+    if len(illegal_params) > 0:
+        s = ''
+        for arg in illegal_params:
+            s += f'Illegal value: "{illegal_params[arg]}" found for {arg}.  Permitted values: {allowed_values[arg]}\n'
+        message += s
     return message
 
 def list_to_dict(a_list):
@@ -149,18 +198,23 @@ mode = get_mode()
 
 missing_params = []
 unknown_params = []
+illegal_params = {}
 
 if mode == CLI:
     missing_params = missing_parameters(prog_arg_dict().keys(), mandatory_options)
     unknown_params = missing_parameters(allowed_options(), prog_arg_dict().keys())
     missing_conditional_params = missing_conditional_parameters()
-    if len(missing_params) > 0 or len(unknown_params) > 0 or len(missing_conditional_params) > 0:
+    illegal_params = check_values()
+    if len(missing_params) > 0 or len(unknown_params) > 0 or len(missing_conditional_params) > 0 \
+        or len(illegal_params) > 0:
         mode = ABORT
     else:
         params = cli_params()
 
 if mode == GIT:
     params = git_params()
+    print("Unclear what to do with GIT mode.  Would prefer it to be flagged explicitly anyway (not just guessed at)")
+    exit()
 
 if mode == ABORT:
     print(help())
