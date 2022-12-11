95f9f9a03f3093617211294019c7293ec10fd421
Marc Gueury <marc.gueury@oracle.com>
2022-12-11 21:02:39 +0100

    2022-12-11

--- option/app_src/ords/build_app.sh
+++ option/app_src/ords/build_app.sh
@@ -11,11 +11,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/../bin/build_common.sh
 
 # Replace the ORDS URL
-sed -i "s&##ORDS_URL##&$ORDS_URL&" nginx_app.locations
 sed -i "s&##ORDS_URL##&$ORDS_URL&" ../oke/ingress-app.yaml
 
 if [ "$TF_VAR_deploy_strategy" == "compute" ]; then
   cp nginx_app.locations ../compute
+  sed -i "s&##ORDS_URL##&$ORDS_URL&" ../compute/nginx_app.locations
 else
   echo "No docker image needed"
 fi  
