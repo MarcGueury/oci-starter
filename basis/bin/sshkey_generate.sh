d3e28964da9e7545a7470a006e494af3769eecc5
Marc Gueury <marc.gueury@oracle.com>
2022-12-12 22:26:26 +0100

    2022-12-12

--- basis/bin/sshkey_generate.sh
+++ basis/bin/sshkey_generate.sh
@@ -7,6 +7,7 @@ FILE=target/ssh_key_starter
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
+    mkdir -p target
     echo "Generating SSH KEY"
     ssh-keygen -b 2048 -t rsa -f $FILE -q -N ""
     chmod 600 $FILE*
