0b8c52c11f1b6dd5c017552bbabc80e56d0bfb68
Marc Gueury <marc.gueury@oracle.com>
2022-12-16 22:02:27 +0100

    v1.3

---
+++ option/src/app/fn/fn_go_none/func.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	fdk "github.com/fnproject/fdk-go"
+	"os"
+)
+
+type Dept struct {
+    Deptno string `json:"deptno"`
+    Dname string `json:"dname"`
+    Loc string `json:"loc"`
+}
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+    var d = []Dept{
+        Dept{Deptno: "10", Dname: "ACCOUNTING", Loc: "Seoul"},
+        Dept{Deptno: "20", Dname: "RESEARCH", Loc: "Cape Town"},
+        Dept{Deptno: "30", Dname: "SALES", Loc: "Brussels"},
+        Dept{Deptno: "40", Dname: "OPERATIONS", Loc: "San Francisco"},
+    }     
+	json.NewEncoder(out).Encode(&d)
+}
