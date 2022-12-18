6e26c4a3fd0fbd8ac908ff8f496eed4fad9d3193
Marc Gueury <marc.gueury@oracle.com>
2022-12-18 03:12:34 +0100

    v1.3

--- option/src/app/php/src/install.sh
+++ option/src/app/php/src/install.sh
@@ -4,7 +4,10 @@ cd $SCRIPT_DIR
 
 # Install last version of PHP
 sudo yum install -y oracle-release-el7
-sudo yum install rh-php70-php rh-php70
+sudo yum install -y oracle-php-release-el7
+sudo yum install -y php
+sudo yum install -y php-oci8-19c
+
 # sudo yum install -y php php-mysql php-json php-fpm
 # ORACLE Instant Client
 sudo yum install -y oracle-instantclient-release-el7
