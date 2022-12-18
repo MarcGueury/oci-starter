6cc2aa9439aebe8dbd85c077565675d8de0b343f
Marc Gueury <marc.gueury@oracle.com>
2022-12-18 10:49:33 +0100

    v1.3

--- option/src/app/php/build_app.sh
+++ option/src/app/php/build_app.sh
@@ -16,7 +16,7 @@ if [ "$TF_VAR_deploy_strategy" == "compute" ]; then
   mkdir -p ../../target/compute/app
   cp -r src/* ../../target/compute/app/.
   # Replace the user and password in the start file
-  replace_db_user_password_in_file ../../target/compute/app/start.sh
+  replace_db_user_password_in_file ../../target/compute/app/php.ini.append
 else
   docker image rm app:latest
   docker build -t app:latest .
