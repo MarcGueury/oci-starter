8ee2e4546ab628f3f811f44a5fcd1b80594fb8fc
Marc Gueury <marc.gueury@oracle.com>
2022-12-18 09:06:56 +0100

    v1.3

--- option/src/app/php/src/install.sh
+++ option/src/app/php/src/install.sh
@@ -14,5 +14,10 @@ sudo yum install -y oracle-instantclient-release-el7
 sudo yum install -y oracle-instantclient-basic
 sudo yum install -y oracle-instantclient-sqlplus
 
-sudo mkdir -p /usr/share/nginx/app
-sudo cp -r app/* /usr/share/nginx/app/.
+# PHP use apache 
+sudo cp app/* /var/www/html/.
+
+# Configure the Apache Listener on 8080
+sed -i "s/Listen 80$/Listen 8080/" /etc/httpd/conf/httpd.conf
+sudo systemctl restart httpd
+
