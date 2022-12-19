ac1d6d4ad7b16092c4105b91d3fded83deba48b5
Marc Gueury <marc.gueury@oracle.com>
2022-12-19 18:22:27 +0100

    v1.3

--- option/src/app/php_none/src/html/dept.php
+++ option/src/app/php_none/src/html/dept.php
@@ -1,34 +1,11 @@
 <?php
-
-class Dept
-{
-    public $depno;
-    public $dname;
-    public $loc;
-}
-
 // Initialize an array to store the data
 $data = array();
-$d = new Dept();
-$d->deptno = '10';
-$d->dname = 'ACCOUNTING';
-$d->loc = 'Seoul';
-$data[] = $row;
-$d = new Dept();
-$d->deptno = '20';
-$d->dname = 'RESEARCH';
-$d->loc = 'Cape Town';
-$data[] = $row;$d = new Dept();
-$d->deptno = '30';
-$d->dname = 'SALES';
-$d->loc = 'Brussels';
-$data[] = $row;$d = new Dept();
-$d->deptno = '40';
-$d->dname = 'OPERATIONS';
-$d->loc = 'San Francisco';
-$data[] = $row;
+$data[] = array("deptno"=>"10", "dname"=>"ACCOUNTING", "loc"=>"Seoul");
+$data[] = array("deptno"=>"20", "dname"=>"RESEARCH", "loc"=>"Seoul");
+$data[] = array("deptno"=>"30", "dname"=>"SALES", "loc"=>"Cape Town");
+$data[] = array("deptno"=>"40", "dname"=>"OPERATIONS", "loc"=>"San Francisco");
 
 // Encode the array as JSON and output it
 echo json_encode($data);
-
 ?>
