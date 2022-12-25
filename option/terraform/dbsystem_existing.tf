9a5c61d9f76da9393740278cfea58210cd469971
Marc Gueury <marc.gueury@oracle.com>
2022-12-25 18:29:26 +0100

    v1.3

--- option/terraform/dbsystem_existing.tf
+++ option/terraform/dbsystem_existing.tf
@@ -2,13 +2,13 @@
 variable "db_ocid" {}
 
 # OCID of the COMPARTMENT of the DBSYSTEM (usecase where it is <> Landing Zone DB Compartment )
-variable "db_compartement_ocid" { default="" }
+variable "db_compartment_ocid" { default="" }
 
 locals {
-  db_compartement_ocid = var.db_compartement_ocid == "" ? local.lz_database_cmp_ocid : var.db_compartement_ocid
+  db_compartment_ocid = var.db_compartment_ocid == "" ? local.lz_database_cmp_ocid : var.db_compartment_ocid
 }
 
 data "oci_database_db_homes" "starter_db_homes" {
-  compartment_id = local.db_compartement_ocid
+  compartment_id = local.db_compartment_ocid
   db_system_id   = var.db_ocid
 }
