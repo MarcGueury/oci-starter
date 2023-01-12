6f0556654da2336c8aba93cd91aa055ad0824d6f
Marc Gueury <marc.gueury@oracle.com>
2023-01-12 15:47:37 +0100

    v1.3

--- option/terraform/apigw.tf
+++ option/terraform/apigw.tf
@@ -1,4 +1,4 @@
-variable "TF_VAR_openapi_spec" {
+variable "openapi_spec" {
   default = "openapi: 3.0.0\ninfo:\n  version: 1.0.0\n  title: Test API\n  license:\n    name: MIT\npaths:\n  /ping:\n    get:\n      responses:\n        '200':\n          description: OK"
 }
 
@@ -12,7 +12,7 @@ resource oci_apigateway_gateway starter_apigw {
 
 resource "oci_apigateway_api" "starter_api" {
   compartment_id = local.lz_appdev_cmp_ocid
-  content       = var.api_content
+  content       = var.openapi_spec
   display_name  = "${var.prefix}-api"
   freeform_tags = local.freeform_tags
 }
