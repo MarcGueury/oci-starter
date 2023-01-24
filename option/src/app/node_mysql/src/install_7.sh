23aa52064f81b51ff0ee3544553645e79cbfae6e
Marc Gueury <marc.gueury@oracle.com>
2023-01-24 11:25:34 +0100

    v1.5

--- option/src/app/node_mysql/src/install_7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-
-# Install last version of NodeJS
-sudo yum install -y oracle-nodejs-release-el7 oracle-release-el7
-sudo yum install -y nodejs
-npm install
--- option/src/app/python/src/install_7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-
-sudo yum install -y python3 python3-devel
-sudo pip3 install -r requirements.txt
